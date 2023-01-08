f7a520db111171ad8395f3cddf6a8fa3852d3f17
unknown <murmuparmeshwar05@gmail.com>
2023-01-08 20:58:11 +0530

    Issue Fixed

---
+++ palindromeUpdated.js
@@ -0,0 +1,15 @@
+let sample="mam";
+
+let a="";
+
+for(let i=sample.length-1 ;i>=0;i--){
+a=a+sample[i];
+}
+
+if(sample==a){
+console.log("Palindrome");
+}
+else
+{
+console.log("Not Palindrome);
+}
